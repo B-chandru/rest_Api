e17f3a29ddc9a169a2cee93f75ae9bef979c43d3
Chandru <65664480+B-chandru@users.noreply.github.com>
2021-06-16 17:24:19 +0530

    Update crud.js

--- controller/crud.js
+++ controller/crud.js
@@ -18,7 +18,7 @@ const crudCtrl={
             res.status(200).json(crud);
         }
         catch(err){
-         res.status(409).json({ err: err.message})
+         res.status(404).json({ err: err.message})
         }
 
     },
